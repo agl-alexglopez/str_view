96b962d33eef7441c144fd6ddf294ba729a69b9d
Alexander Lopez <agl.alexglopez@gmail.com>
2024-07-15 16:57:47 -0700

    generate change on c file

--- str_view/str_view.c
+++ str_view/str_view.c
@@ -4,7 +4,7 @@
    This file implements the str_view interface as an approximation of C++
    std::string_view type. There are some minor differences and C flavor thrown
    in. Additionally, there is a provided reimplementation of the Two-Way
-   String-Searching algorithm, similar to glibc. */
+   String-Searching algorithm, similar to glibc that is under development. */
 #include "str_view.h"
 
 #include <stdbool.h>
