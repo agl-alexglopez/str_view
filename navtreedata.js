6cdfc8a7031cd5cecdd1842c43ebebb71376322d
agl-alexglopez <agl.alexglopez@gmail.com>
2025-12-10 18:08:12 +0000

    Deploying to gh-pages from @ SkeletOSS/str_view@272279c354957421dadad21c7925f13f74849057 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -28,13 +28,12 @@ var NAVTREE =
     [ "Interface", "index.html#autotoc_md1", null ],
     [ "Install", "index.html#autotoc_md2", null ],
     [ "Building and Installation", "md_INSTALL.html", [
-      [ "VCPKG", "md_INSTALL.html#autotoc_md4", null ],
-      [ "Fetch Content", "md_INSTALL.html#autotoc_md5", null ],
-      [ "Manual Install Quick Start", "md_INSTALL.html#autotoc_md6", null ],
-      [ "Include the Library", "md_INSTALL.html#autotoc_md7", null ],
-      [ "Alternative Builds", "md_INSTALL.html#autotoc_md8", null ],
-      [ "Without Make", "md_INSTALL.html#autotoc_md9", null ],
-      [ "User Presets", "md_INSTALL.html#autotoc_md10", null ]
+      [ "Fetch Content", "md_INSTALL.html#autotoc_md4", null ],
+      [ "Manual Install Quick Start", "md_INSTALL.html#autotoc_md5", null ],
+      [ "Include the Library", "md_INSTALL.html#autotoc_md6", null ],
+      [ "Alternative Builds", "md_INSTALL.html#autotoc_md7", null ],
+      [ "Without Make", "md_INSTALL.html#autotoc_md8", null ],
+      [ "User Presets", "md_INSTALL.html#autotoc_md9", null ]
     ] ],
     [ "Data Structures", "annotated.html", [
       [ "Data Structures", "annotated.html", "annotated_dup" ],
